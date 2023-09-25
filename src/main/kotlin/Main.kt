e1b724a5a7b8163644717b5562f6df0e71ef4a24
Hudhalovenilou <Hudarahmadani@gmail.com>
2023-09-25 08:23:46 +0700

    nol

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+//fungsi utama yang digunakan
+fun main() {
+    //var merupakan code yang dapat ditimpa oleh code lain atau code yang dapat disetel berulang-ulang
+    var myName ="Rahell"
+    //sedangkan val merupakan code yang hanya dapat disetel satu kali atau code yang tidak dapat ditimpa oleh kode lain
+    val myName1 = "Hudha"
+    //print digunakan untuk mencetak output yang telah disediakan atau output yang dibuat dalam fungsi print
+    print(myName)
+    print(myName1)
+    print("Hello world")
+}
